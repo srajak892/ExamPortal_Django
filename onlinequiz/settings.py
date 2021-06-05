fb0468413a9ad535f85d9a5175c1c41542018f03
srajak892 <srajak892@gmail.com>
2021-06-05 10:39:12 +0530

    Django Project last

--- onlinequiz/settings.py
+++ onlinequiz/settings.py
@@ -15,8 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
-STATIC_DIR=os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_DIR=os.path.join(BASE_DIR,'statics')
+MEDIA_ROOT=os.path.join(BASE_DIR,'statics')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -136,16 +136,16 @@ STATIC_DIR,
 
 LOGIN_REDIRECT_URL='/afterlogin'
 
-#for contact us give your gmail id and password
-EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'from@gmail.com' # this email will be used to send emails
-EMAIL_HOST_PASSWORD = 'xyz' # host email password required
-# now sign in with your host gmail account in your browser
-# open following link and turn it ON
-# https://myaccount.google.com/lesssecureapps
-# otherwise you will get SMTPAuthenticationError at /contactus
-# this process is required because google blocks apps authentication by default
-EMAIL_RECEIVING_USER = ['srajak892@gmail.com'] # email on which you will receive messages sent from website
+# #for contact us give your gmail id and password
+# EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'from@gmail.com' # this email will be used to send emails
+# EMAIL_HOST_PASSWORD = 'xyz' # host email password required
+# # now sign in with your host gmail account in your browser
+# # open following link and turn it ON
+# # https://myaccount.google.com/lesssecureapps
+# # otherwise you will get SMTPAuthenticationError at /contactus
+# # this process is required because google blocks apps authentication by default
+# EMAIL_RECEIVING_USER = ['srajak892@gmail.com'] # email on which you will receive messages sent from website
